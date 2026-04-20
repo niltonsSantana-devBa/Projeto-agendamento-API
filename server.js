5f8d02edfc55d24ad9569a0905b06b8f4e35e682
niltonsSantana-devBa <nilton.s.santana@ba.estudante.senai.br>
2026-04-20 18:53:19 -0300

    feat: usar variaveis de ambiente para credenciais do banco de dados

--- server.js
+++ server.js
@@ -1,34 +1,51 @@
+require('dotenv').config();
 const express = require('express');
-const mysql = require('mysql2'); // Puxando o "tradutor" do banco de dados
+const mysql = require('mysql2');
 
 const app = express();
 const porta = 3000;
 
 app.use(express.json());
 
-// 🗄️ Criando a conexão com o banco de dados
+
 const conexao = mysql.createConnection({
-    host: 'localhost', // O banco está no seu computador
-    user: 'root',      // Seu usuário do MySQL (geralmente é root)
-    password: 'NILton@1112',      // Sua senha do MySQL (se tiver senha no Workbench, coloque aqui!)
-    database: 'mydb'   // O nome do seu banco (conforme estava na sua foto)
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
-// 🔍 Testando se a conexão deu certo
+
 conexao.connect((erro) => {
     if (erro) {
-        console.error('❌ Erro ao conectar com o banco de dados:', erro);
+        console.error(' Erro ao conectar com o banco de dados:', erro);
         return;
     }
-    console.log('✅ Conectado ao banco de dados MySQL com sucesso! 🗄️');
+    console.log(' Conectado ao banco de dados MySQL com sucesso! ');
 });
 
-// Rota principal (aquela do navegador)
+
 app.get('/', (req, res) => {
     res.send('Fala, dev! O servidor da Clínica de Arquitetura está ON e conectado ao banco! 🚀');
 });
 
-// Ligando o servidor
+
+const porta = process.env.PORT || 3000;
 app.listen(porta, () => {
     console.log(`🌐 Servidor rodando bonitão em http://localhost:${porta}`);
+});
+
+
+app.get('/clientes', (req, res) => {
+   
+    const comandoSql = 'SELECT * FROM clientes';
+
+    conexao.query(comandoSql, (erro, resultados) => {
+        if (erro) {
+            console.error(' Erro ao buscar os clientes:', erro);
+            res.status(500).send('Deu erro ao tentar buscar os dados.');
+            return;
+        }
+        res.json(resultados);
+    });
 });
